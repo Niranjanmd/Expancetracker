49a0fa0e98e68675c5652ced08d325d5619a045e
Niranjan <niranjanmd09@gmail.com>
2018-01-06 17:41:34 +0530

    adding list to dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Bankservice } from './../../services/bank.service';
 import { Component, OnInit } from '@angular/core';
+import { ExpenceService } from './../../services/expence.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,12 +10,34 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   banks: any[];
   totalBal: number;
-  constructor(private bs: Bankservice) { }
+  totalDebt: number;
+  expanceList: any[];
+  constructor(private bs: Bankservice, private es: ExpenceService) { }
 
   ngOnInit() {
     this.bs.getBankList().subscribe(res => {
       this.banks = res;
       this.getTotalBalance();
+    });
+
+    this.getCurrentMonthTotalExp();
+  }
+  getCurrentMonthTotalExp() {
+    const d = new Date();
+    const m = d.getMonth() + 1;
+    const y = d.getFullYear();
+    console.log(m + ' ' + y);
+
+    this.es.getExpenceList( y.toString() , m.toString()).subscribe(res => {
+      let total = 0;
+      this.expanceList = res;
+      res.forEach(function(item){
+        if (!item.isPaid) {
+          total += item.amount;
+        }
+      });
+      this.totalDebt = total;
+      // console.log(this.totalDebt);
     });
   }
 
@@ -28,6 +51,6 @@ export class DashboardComponent implements OnInit {
 
     }
     this.totalBal = total;
-    console.log(total);
+    // console.log(total);
   }
 }
--- src/app/components/expance-list/expance-list.component.ts
+++ src/app/components/expance-list/expance-list.component.ts
@@ -4,22 +4,44 @@ import { Year } from './../../models/year';
 import { ExpenceService } from './../../services/expence.service';
 import { Component, OnInit } from '@angular/core';
 
+const months: Month[] = [
+  { id: '1', value: 'Jan' },
+  { id: '2', value: 'Feb' },
+  { id: '3', value: 'Mar' },
+  { id: '4', value: 'Apr' },
+  { id: '5', value: 'May' },
+  { id: '6', value: 'Jun' },
+  { id: '7', value: 'Jul' },
+  { id: '8', value: 'Aug' },
+  { id: '9', value: 'Sep' },
+  { id: '10', value: 'Oct' },
+  { id: '11', value: 'Nov' },
+  { id: '12', value: 'Dec' },
+];
+
 @Component({
   selector: 'app-expance-list',
   templateUrl: './expance-list.component.html',
   styleUrls: ['./expance-list.component.css']
 })
+
+
 export class ExpanceListComponent implements OnInit {
   expanceList: any[];
   expr: any[];
   years: Year[]= [ ];
-  months: string[]= [];
-  expyear: any[];
-  selectedYear: string;
-  selectedMonth: string;
-  isYearNotSelected = true;
+  // months: string[]= [];
 
-  constructor(private es: ExpenceService) { }
+  selectedYear= '0';
+  selectedMonth= '0';
+  isYearNotSelected = false;
+  months: Month[] = months;
+  date = new Date;
+
+  constructor(private es: ExpenceService) {
+    this.selectedYear = this.date.getFullYear().toString();
+    this.selectedMonth = (this.date.getMonth() + 1).toString();
+   }
 
 
   ngOnInit() {
@@ -27,9 +49,9 @@ export class ExpanceListComponent implements OnInit {
     this.es.getYears().subscribe(res => {
       this.expr = res;
       this.getYear();
-      console.log(this.expr);
     });
 
+    this.getExpenceList(this.selectedYear, this.selectedMonth);
   }
 
 
@@ -49,18 +71,23 @@ export class ExpanceListComponent implements OnInit {
     // console.log(this.expr);
     const year = this.selectedYear;
     // let seleObj: any[] ;
-    this.expyear = this.expr.filter(function(obj) {
-      return obj.$key === year;
-    });
+    // this.expyear = this.expr.filter(function(obj) {
+    //   return obj.$key === year;
+    // });
 
     // console.log(seleObj);
     // console.log(Object.keys(seleObj[0]));
-    for (let index = 0; index < this.expyear.length; index++) {
-      this.months = Object.keys(this.expyear[index]);
-    }
+    // for (let index = 0; index < this.expyear.length; index++) {
+    //   this.months = Object.keys(this.expyear[index]);
+    // }
   }
 
   onMonthChange() {
+    this.getExpenceList(this.selectedYear, this.selectedMonth);
+  }
+
+
+  getExpenceList(year: string, month: string){
     this.es.getExpenceList(this.selectedYear, this.selectedMonth).subscribe(res => {
       this.expanceList = res;
       // console.log(this.expanceList);
